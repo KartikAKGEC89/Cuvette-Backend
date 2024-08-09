3ba141d8aa5aa9204b6dba84fa4d1e40e37b8846
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-08-09 12:09:22 +0530

    Modify add route index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser=require('body-parser');
 const cors=require('cors');
 
 const connectDB=require('./config/databaseConnection');
+const { createGroup, getAllGroups, createNote, getAllNotes } = require('./controller/Backendcontroller');
 app.use(cors({
   origin:'*'
 }))
@@ -14,6 +15,11 @@ connectDB();
 app.get('/', (req, res) => {
     res.send('Hello');
 });
+
+app.post('/groups/create',createGroup);
+app.get('/groups/all', getAllGroups);
+app.post('/notes/create',createNote);
+app.get('/notes/all/:groupId',getAllNotes);
 
 app.listen(3000, () => {
     console.log('Your server running on port 3000');
